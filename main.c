fb189d508cd4c79ffc82c5cfd539c0af2188e4ab
“MDDO0” <maado321@gmail.com>
2025-04-21 20:42:43 +0000

    a

--- main.c
+++ main.c
@@ -4,17 +4,17 @@ extern char **environ;
 
 int main(void)
 {
-	char *line = NULL, *trimmed_line, *token;
-	char *argv[64];
+	char *line = NULL, *trimmed, *token, *full_cmd;
 	size_t len = 0;
 	ssize_t read;
 	pid_t pid;
+	char *argv[64];
 	int i;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "#cisfun$ ", 9);
+			write(STDOUT_FILENO, ":) ", 3);
 
 		read = getline(&line, &len, stdin);
 		if (read == -1)
@@ -28,12 +28,12 @@ int main(void)
 		if (line[read - 1] == '\n')
 			line[read - 1] = '\0';
 
-		trimmed_line = trim_whitespace(line);
-		if (trimmed_line[0] == '\0')
+		trimmed = trim_whitespace(line);
+		if (*trimmed == '\0')
 			continue;
 
 		i = 0;
-		token = strtok(trimmed_line, " \t");
+		token = strtok(trimmed, " \t");
 		while (token != NULL && i < 63)
 		{
 			argv[i++] = token;
@@ -41,28 +41,35 @@ int main(void)
 		}
 		argv[i] = NULL;
 
-		if (argv[0] == NULL)
+		full_cmd = find_path(argv[0]);
+		if (full_cmd == NULL)
+		{
+			perror("./hsh");
 			continue;
+		}
 
 		pid = fork();
 		if (pid == -1)
 		{
 			perror("fork");
 			free(line);
+			free(full_cmd);
 			exit(EXIT_FAILURE);
 		}
 
 		if (pid == 0)
 		{
-			if (execve(argv[0], argv, environ) == -1)
+			if (execve(full_cmd, argv, environ) == -1)
 			{
 				perror("./hsh");
+				free(full_cmd);
 				exit(EXIT_FAILURE);
 			}
 		}
 		else
 		{
 			wait(NULL);
+			free(full_cmd);
 		}
 	}
 	free(line);
