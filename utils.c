67d1fd5a0dd8dba5efa04f167289a0132be87962
“MDDO0” <maado321@gmail.com>
2025-04-21 20:42:56 +0000

    a

--- utils.c
+++ utils.c
@@ -17,3 +17,44 @@ char *trim_whitespace(char *str)
 	*(end + 1) = '\0';
 	return str;
 }
+
+char *find_path(char *command)
+{
+	char *path = getenv("PATH");
+	char *path_copy, *token, *full_path;
+	size_t len;
+
+	if (command == NULL || path == NULL)
+		return NULL;
+
+	if (access(command, X_OK) == 0)
+		return strdup(command);
+
+	path_copy = strdup(path);
+	token = strtok(path_copy, ":");
+
+	while (token != NULL)
+	{
+		len = strlen(token) + strlen(command) + 2;
+		full_path = malloc(len);
+		if (full_path == NULL)
+		{
+			free(path_copy);
+			return NULL;
+		}
+
+		snprintf(full_path, len, "%s/%s", token, command);
+
+		if (access(full_path, X_OK) == 0)
+		{
+			free(path_copy);
+			return full_path;
+		}
+
+		free(full_path);
+		token = strtok(NULL, ":");
+	}
+
+	free(path_copy);
+	return NULL;
+}
