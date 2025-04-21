bc2c5568457fa991a028f3951e4f9cf51b61b9f3
“MDDO0” <maado321@gmail.com>
2025-04-21 20:23:12 +0000

    a

---
+++ main.c
@@ -0,0 +1,57 @@
+#include "shell.h"
+
+extern char **environ;
+
+/**
+ * main - Simple UNIX command line interpreter (0.1)
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	pid_t pid;
+	char *argv[2];
+
+	while (1)
+	{
+		write(STDOUT_FILENO, "#cisfun$ ", 9);
+		read = getline(&line, &len, stdin);
+
+		if (read == -1)
+		{
+			free(line);
+			write(STDOUT_FILENO, "\n", 1);
+			return (0);
+		}
+
+		if (line[read - 1] == '\n')
+			line[read - 1] = '\0';
+
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("fork");
+			free(line);
+			exit(EXIT_FAILURE);
+		}
+		if (pid == 0)
+		{
+			argv[0] = line;
+			argv[1] = NULL;
+			if (execve(argv[0], argv, environ) == -1)
+			{
+				perror("./hsh");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			wait(NULL);
+		}
+	}
+	free(line);
+	return (0);
+}
