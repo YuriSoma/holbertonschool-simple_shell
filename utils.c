57273371707e050039eb41cb28b4de8ae0d8961c
“MDDO0” <maado321@gmail.com>
2025-04-21 20:33:20 +0000

    a

---
+++ utils.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+char *trim_whitespace(char *str)
+{
+	char *end;
+
+	while (*str == ' ' || *str == '\t' || *str == '\n')
+		str++;
+
+	if (*str == '\0')
+		return str;
+
+	end = str + strlen(str) - 1;
+	while (end > str && (*end == ' ' || *end == '\t'))
+		end--;
+
+	*(end + 1) = '\0';
+	return str;
+}
