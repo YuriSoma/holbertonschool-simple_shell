d3dc8246c5f7c19d08de7a55c24c49c1c1798771
“MDDO0” <maado321@gmail.com>
2025-04-23 10:29:44 +0000

    a

--- utils.c
+++ utils.c
@@ -1,6 +1,95 @@
 #include "shell.h"
 
 /**
+ * trim_whitespace - removes leading and trailing whitespaces
+ * @str: input string
+ *
+ * Return: trimmed string
+ */
+char *trim_whitespace(char *str)
+{
+	char *end;
+
+	while (*str == ' ' || *str == '\t' || *str == '\n')
+		str++;
+
+	if (*str == '\0')
+		return (str);
+
+	end = str + strlen(str) - 1;
+	while (end > str && (*end == ' ' || *end == '\t'))
+		end--;
+
+	*(end + 1) = '\0';
+	return (str);
+}
+
+/**
+ * get_path_env - gets PATH environment variable
+ *
+ * Return: pointer to PATH string
+ */
+char *get_path_env(void)
+{
+	int i = 0;
+
+	while (environ[i])
+	{
+		if (strncmp(environ[i], "PATH=", 5) == 0)
+			return (environ[i] + 5);
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * find_path - searches for a command in PATH
+ * @command: command to find
+ *
+ * Return: full path or NULL
+ */
+char *find_path(char *command)
+{
+	char *path = get_path_env();
+	char full_path[1024];
+	char *token, *ptr;
+	int len;
+
+	if (!command)
+		return (NULL);
+
+	if (strchr(command, '/'))
+	{
+		if (access(command, X_OK) == 0)
+			return (strdup(command));
+		else
+			return (NULL);
+	}
+
+	if (!path || path[0] == '\0')
+		return (NULL);
+
+	ptr = path;
+	while (*ptr)
+	{
+		token = ptr;
+		while (*ptr && *ptr != ':')
+			ptr++;
+
+		len = ptr - token;
+		snprintf(full_path, sizeof(full_path), "%.*s/%s", len, token, command);
+
+		if (access(full_path, X_OK) == 0)
+			return (strdup(full_path));
+
+		if (!*ptr)
+			break;
+		ptr++;
+	}
+	return (NULL);
+}
+
+/**
  * handle_builtins - handles built-in commands (exit, env)
  * @argv: array of arguments
  *
